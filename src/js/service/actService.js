bf07e51493c4c22ce5218af46c4d3237fd3bbc1f
Aliksei <kikit228@gmail.com>
2021-01-25 14:37:26 +0300

    READY

--- src/js/service/actService.js
+++ src/js/service/actService.js
@@ -1,7 +1,6 @@
 import {extendedFetcher} from "../rest/fetcher";
 
 export const API_HOST = process.env.REACT_APP_SPRING_BASE_URL;
-// export const API_HOST = "localhost";
 
 
 export const getActs = async (signal) => {
